f5d43b299af1f4f99c843c9fcf0dfc37b9417bdd
Masahiro Kiyota <hiro@juzbox.com>
2015-04-19 11:15:46 +0900

    Initial Commit

---
+++ LrcShow/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LrcShow
+//
+//  Created by Hiro on 4/19/15.
+//  Copyright (c) 2015 Juzbox. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ LrcShowTests/LrcShowTests.m
@@ -0,0 +1,40 @@
+//
+//  LrcShowTests.m
+//  LrcShowTests
+//
+//  Created by Hiro on 4/19/15.
+//  Copyright (c) 2015 Juzbox. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <XCTest/XCTest.h>
+
+@interface LrcShowTests : XCTestCase
+
+@end
+
+@implementation LrcShowTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
